5a9140188396a96afe1588b133e3bebba6dcbfc8
Hayden Corderio <cordeirohayden@gmail.com>
2020-09-29 14:34:35 +0530

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname')->nullable();
+            $table->string('lname')->nullable();
+            $table->string('SID')->unique()->nullable();
+            $table->string('phoneno')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('year', ['first year','second year','third year','fourth year']);
+            
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::dropIfExists('users');
+    }
+}
