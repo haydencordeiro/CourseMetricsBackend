ae7eec9470f3ec5f58fa5fd2a849ecc2d3b431f0
Hayden Corderio <cordeirohayden@gmail.com>
2020-10-21 11:14:07 +0530

    teacher home bug solved

--- resources/views/teacherHome.blade.php
+++ resources/views/teacherHome.blade.php
@@ -22,26 +22,136 @@
     <h2 style="margin: 1% 2.5% 0 2.5%;">Marks Analysis</h2>
     <form method="POST"  id="studentIdsform" action="/attendanceForm">
         @csrf
-        <div class="table_search cardRow1 tablecard" style="justify-content: flex-start;">
+<div class="table_search cardRow1 tablecard" style="justify-content: flex-start;">
 
 
+<div>
 
+
+    <div class="dropdown">
+        <div class="custom-select">
+            <select name="deptSelect">
+            <option value="Comps">Dept</option>
+            <option value="Comps">Comps</option>
+            <option value="Mech">Mech</option>
+            <option value="IT">IT</option>
+            <option value="EXTC">EXTC</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <div class="custom-select">
+            <select name="classSelect">
+            <option value="A">Class</option>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+
+
+            </select>
+        </div>
+    </div>
+    <div class="dropdown">
+        <div class="custom-select">
+            <select name="slotSelect">
+            <option value="0">Slot</option>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+
+
+            </select>
+        </div>
+    </div>
+    <div class="dropdown">
+        <div class="custom-select">
+            <select name="subjSelect">
+            <option value="MP">Subject</option>
+            @foreach($subjectList as $sub)
+            <option value="{{$sub->SubjectName}}">{{$sub->SubjectName}}</option>
+
+            @endforeach
+
+            </select>
+        </div>
+    </div>
+</div>
+
+    {{-- <div>
+        <div class="dropdown">
+            <button class="dropbtn">Branch<i class="fa fa-caret-down" aria-hidden="true"
+                    style="margin-left: 10px;"></i></button>
+            <div class="dropdown-content">
+                <a href="#">Computer</a>
+                <a href="#">IT</a>
+                <a href="#">EXTC</a>
+                <a href="#">Mechanical</a>
+            </div>
+        </div>
+    </div> --}}
+    {{-- <div>
+        <div class="dropdown">
+            <button class="dropbtn">Subject<i class="fa fa-caret-down" aria-hidden="true"
+                    style="margin-left: 10px;"></i></button>
+            <div class="dropdown-content" name="test" id="test">
+                <a href="#">MP</a>
+                <a href="#">CN</a>
+                <a href="#">TCS</a>
+                <a href="#">AA</a>
+            </div>
+        </div>
+    </div> --}}
+    
+    <div>
+        <div class="dropdown">
+            <input type="date" name="attDate" id="attDate" class="dropinput" style="outline: none;">
+            <!-- <button class="dropbtn">Date</button> -->
+            <!-- <div class="dropdown-content"> -->
+            <!-- <a href="#">First Year</a> -->
+            <!-- <a href="#">Second Year</a> -->
+            <!-- <a href="#">Third Year</a> -->
+            <!-- <a href="#">Fourth Year</a> -->
+        </div>
+    </div>
+    {{-- <div>
+        <div class="dropdown">
+            <input type="time" name="fromtime" id="fromtime" class="dropinput" style="outline: none;" placeholder="From">
+        </div>
+    </div>
+    <div>
+        <div class="dropdown">
+            <input type="time" name="" id="" class="dropinput" style="outline: none;" placeholder="To">
+        </div>
+    </div> --}}
+    <div>
+        <button onclick="onlySearch();">Go</button>
+    </div>
+    
+    <!-- <div style="flex: 0 0 40%;margin-right: 3%;margin-top:2%;">
+                <input type="text" id="search_box" placeholder="Search ..">
+            </div> -->
+    <!-- <div>
                 <div class="dropdown">
-                    <div class="custom-select">
-                        <select name="deptSelect">
-                        <option value="Comps">Dept</option>
-                        <option value="Comps">Comps</option>
-                        <option value="Mech">Mech</option>
-                        <option value="IT">IT</option>
-                        <option value="EXTC">EXTC</option>
-                        </select>
-                    </div>
+                    <button class="dropbtn btn-success">Present</button>
                 </div>
             </div>
-
-            <input type="input" style="display:none" id="studentIds" name="studentIds" value="">
-            <input type="input" style="display:none" id="checkWhich" name="checkWhich" value="">
-    </form>
+            <div>
+                <div class="dropdown">
+                    <button class="dropbtn btn-danger">Absent</button>
+                </div>
+            </div> -->
+</div>
+<input type="input" style="display:none" id="studentIds" name="studentIds" value="">
+<input type="input" style="display:none" id="checkWhich" name="checkWhich" value="">
+</form>
     <div class="flexContainer">
         <div class="flex-50 chartCard card">
             <h4> <strong>Class Performance</strong></h4>
@@ -231,6 +341,128 @@
         options: optionsatt
     });
 
+</script>
+<script>
+
+
+    function getSelected(){
+        var checkbox=document.getElementsByClassName('attcheck');
+        var checkedList=[]
+        for(var i=0;i<checkbox.length;i++){
+            if(checkbox[i].checked){
+                
+            checkedList.push(parseInt(checkbox[i].id))
+            }
+        }
+        console.log(checkedList);
+
+        document.getElementById('studentIds').value='['+checkedList+']';
+        document.getElementById('checkWhich').value='Atten';
+
+
+        document.getElementById('studentIdsform').submit();
+    }
+
+    function onlySearch(){
+        document.getElementById('checkWhich').value='Go';
+        document.getElementById('studentIdsform').submit();
+
+
+    }
+    // getSelected();
+
+
+
+
+    var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
+
+    $(document).ready(function () {
+        $("#search_box").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            $("#attendance_form_table_data tr").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
 </script>
 
 
